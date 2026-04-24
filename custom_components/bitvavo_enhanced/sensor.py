16a4eaa276b28c1f7394df99fb14d7ce8051c44f
Paul Zoetemeijer <paul_zoetemeijer@hotmail.com>
2026-04-25 01:02:20 +0200

    EUR sensors added

--- custom_components/bitvavo_enhanced/sensor.py
+++ custom_components/bitvavo_enhanced/sensor.py
@@ -1,3 +1,4 @@
+
 import logging
 
 from homeassistant.components.sensor import SensorEntity, SensorStateClass
@@ -11,10 +12,14 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
 
-    entities = [
-        BitvavoAssetSensor(coordinator, asset)
-        for asset in coordinator.data.get(CONF_BALANCES, {})
-    ]
+    entities = []
+
+    for asset in coordinator.data.get(CONF_BALANCES, {}):
+        entities.append(BitvavoAssetSensor(coordinator, asset))
+        entities.append(BitvavoEurValueSensor(coordinator, asset))
+
+    # Portfolio total sensor
+    entities.append(BitvavoPortfolioTotalSensor(coordinator))
 
     async_add_entities(entities)
 
@@ -54,3 +59,41 @@ class BitvavoAssetSensor(CoordinatorEntity, SensorEntity):
             "eur_value": data.get("eur_value"),
             "attribution": ATTRIBUTION,
         }
+
+
+class BitvavoEurValueSensor(CoordinatorEntity, SensorEntity):
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_native_unit_of_measurement = "€"
+    _attr_device_class = "monetary"
+
+    def __init__(self, coordinator, asset: str):
+        super().__init__(coordinator)
+        self._asset = asset
+        self._attr_name = f"Bitvavo {asset} EUR Value"
+        self._attr_unique_id = f"bitvavo_enhanced_{asset}_eur_value"
+
+    @property
+    def state(self):
+        data = self.coordinator.data[CONF_BALANCES].get(self._asset, {})
+        value = data.get("eur_value")
+        return round(value, 2) if value is not None else 0
+
+
+class BitvavoPortfolioTotalSensor(CoordinatorEntity, SensorEntity):
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_name = "Bitvavo Total EUR"
+    _attr_unique_id = "bitvavo_enhanced_total_eur"
+    _attr_native_unit_of_measurement = "€"
+    _attr_device_class = "monetary"
+
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+
+    @property
+    def state(self):
+        balances = self.coordinator.data.get(CONF_BALANCES, {})
+        total = sum(
+            asset.get("eur_value", 0) or 0
+            for asset in balances.values()
+        )
+        return round(total, 2)
